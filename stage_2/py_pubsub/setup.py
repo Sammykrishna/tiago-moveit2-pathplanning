73fc4daacb735072f59841cbbc0e2243b768c8fb
samanth krishna <sk-246767@hs-weingarten.de>
2025-11-30 18:31:51 +0000

    updated stage_2

--- stage_2/py_pubsub/setup.py
+++ stage_2/py_pubsub/setup.py
@@ -1,27 +1,28 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'py_pubsub'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools', 'rclpy', 'std_msgs'],
+    install_requires=['setuptools'],
     zip_safe=True,
     maintainer='samanth krishna',
     maintainer_email='sk-246767@hs-weingarten.de',
-    description='Python publisher/subscriber example',
+    description='Simple ROS 2 pub/sub example in one file',
     license='Apache-2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'publisher_member_function = py_pubsub.publisher_member_function:main',
-            'subscriber_member_function = py_pubsub.subscriber_member_function:main',
+            
+            'publisher_member_function = py_pubsub.pubsub:main_publisher',
+            'subscriber_member_function = py_pubsub.pubsub:main_subscriber',
         ],
     },
 )
